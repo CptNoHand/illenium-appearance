4a4e060c4f6073970aa8ca43a0d23dbf7ae8f44d
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2023-07-15 17:52:39 +0200

    update

--- shared/blacklist.lua
+++ shared/blacklist.lua
@@ -2,19 +2,57 @@ Config.Blacklist = {
     male = {
         hair = {},
         components = {
-            masks = {},
+            masks = {
+                {
+                    drawables = {209},
+                    jobs = {"fbi"}
+                }
+            },
             upperBody = {},
             lowerBody = {},
             bags = {},
             shoes = {},
-            scarfAndChains = {},
-            shirts = {},
-            bodyArmor = {},
+            scarfAndChains = {
+                {
+                    drawables = {1, 2, 5, 7, 6, 8, 20},
+                    jobs = {"police", "sheriff", "detectives"}
+                }
+            },
+            shirts = {
+                {
+                    drawables = {37, 38, 39, 42, 43, 44, 49, 53, 54, 55, 56, 57, 58, 67, 71, 72, 79, 104, 105, 106, 112, 113, 114, 115, 116, 131, 41, 48, 51, 55, 74, 75, 81, 93, 94, 97, 98, 99, 17, 30, 35},
+                    jobs = {"police", "sheriff", "detectives"}
+                },
+                {
+                    drawables = {191, 262},
+                    jobs = {"fbi"}
+                }
+            },
+            bodyArmor = {
+                {
+                    drawables = {1, 2, 5, 7, 11, 12, 16, 17, 18, 21, 25, 26, 27, 30, 55, 54, 55},
+                    jobs = {"police", "sheriff", "detectives"}
+                }
+            },
             decals = {},
-            jackets = {}
+            jackets = {
+                {
+                    drawables = {17, 31, 32, 35, 36, 39, 41, 48, 51, 55, 74, 75, 81, 93, 94, 97, 98, 99, 100, 101, 102, 103, 111, 118, 123, 154, 156, 166, 183},
+                    jobs = {"police", "sheriff", "detectives"}
+                },
+                {
+                    drawables = {421},
+                    jobs = {"fbi"}
+                }
+            }
         },
         props = {
-            hats = {},
+            hats = {
+                {
+                    drawables = {1, 8, 13, 46, 116, 117, 118, 119, 147, 148},
+                    jobs = {"police", "sheriff", "detectives"}
+                }
+            },
             glasses = {},
             ear = {},
             watches = {},
